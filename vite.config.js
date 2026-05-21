602276c7323ef961dd548e08da7b258ff3d7f766
mimmizzang <fpahs0920@gmail.com>
2026-05-21 13:54:08 +0900

    feat: add notice, notice-detail page and setup Vite MPA config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        notice: resolve(__dirname, "src/pages/notice/notice.html"),
+        noticeDetail: resolve(__dirname, "src/pages/notice/notice-detail.html"),
+      },
+    },
+  },
 });
